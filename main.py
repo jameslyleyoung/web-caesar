12a043e58f2b646920f3614cf6eefb4738036c77
James Lyle Young <jameslyleyoung@gmail.com>
2017-03-24 15:25:04 -0500

    checked encrypt function

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import webapp2
+import caesar
+
 
 class Index(webapp2.RequestHandler):
     def get(self):
-        self.response.write("helllllllllo")
+        message = "Hellooooo world!"
+        encrypted_message = caesar.encrypt(message, 13)
+
+
+
+        self.response.write(encrypted_message)
 
 app = webapp2.WSGIApplication([
     ('/', Index)
